bcc7bbe99a4d3758e874d00401a55101b97c57a0
blake oliver <oliver22213@me.com>
2015-11-29 06:24:56 -0500

    Add cleandir.bat file I stole from another project

---
+++ cleandir.bat
@@ -0,0 +1,9 @@
+@echo off
+erase /S /Q *.pyc
+erase /S /Q *.nvda-addon
+erase /S /Q *.dblite
+erase /S /Q *.ini
+erase /S /Q *.mo
+erase /S /Q *.html
+;rd /S /Q .\addon\doc\en
+erase /Q .\addon\doc\*.css
